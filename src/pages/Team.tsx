a83c23479614e2258a34f258e4e916a72d372053
1303LIZ <mwaranganga@gmail.com>
2025-05-27 13:25:38 +0300

    navbar

---
+++ src/pages/Team.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+import { useEffect, useState } from 'react';
+
+const TeamPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Navbar />
+    </div>
+  );
+};
+
+export default TeamPage;
