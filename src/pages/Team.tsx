66d4c4e38b413bcb80c7573136a0460a90cb0db7
Theetilen19 <ochiengtilen5@gmail.com>
2025-05-28 09:41:40 +0300

    Added some images

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -25,25 +25,32 @@ const TeamPage = () => {
     
     
     {
-      id: 1,
-      name: 'Mahmoud Noor',
-      role: 'CEO, Founder and Chief Mentor',
+      id: 3,
+      name: 'communication',
+      role: 'Communication',
       image: '/img/team/lukaomosh.jpg',
-      note: 'Co-Founder for SwahiliPot Hub Foundation and the Chief Mentor',
+      note: 'In charge of communication both at the Hub and SwahiliPot Hub FM',
     },
     {
-      id: 2,
-      name: 'Miriam',
-      role: 'Communication Department Lead',
+      id: 4,
+      name: 'Tech',
+      role: 'Tech Department Lead',
       image: '/img/team/onyitulia.jpg',
-      note: 'In charge of communication both at the Hub and SwahiliPot Hub FM',
+      note: '',
     },
     
     {
-      id: 8,
+      id: 5,
       name: 'male',
       role: 'Lead, Community Entrepreneurship Department',
       image: '/img/team/male.jpg',
+      note: 'Empowering communities through entrepreneurship initiatives',
+    },
+    {
+      id: 6,
+      name: 'female',
+      role: 'Lead, Community Entrepreneurship Department',
+      image: '/img/team/female.jpg',
       note: 'Empowering communities through entrepreneurship initiatives',
     }
   ];
@@ -59,7 +66,6 @@ const TeamPage = () => {
           <h1 className="text-4xl font-bold text-gray-800 mb-4">Our Team</h1>
           <p className="text-xl text-gray-600 max-w-2xl mx-auto">
           "At Swahilipot Hub, we're driven by a passionate and diverse team of innovators, creators, and visionaries. 
-          Each member brings unique expertise—from tech development and design to strategic leadership—united by a shared mission to empower communities through technology and creativity. 
           Together, we foster collaboration, mentorship, and groundbreaking solutions, ensuring Swahilipot Hub remains a catalyst for growth and opportunity in the digital space."
           </p>
 
