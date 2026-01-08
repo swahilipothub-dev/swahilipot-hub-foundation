9cd660ea58a97e9d25b7c4574e5c4d055f151bb4
Swahilipot Hub Foundation <dev@swahilipothub.co.ke>
2026-01-08 09:55:20 +0000

    add spacing to the TOR

--- client/src/pages/PlacementPartnersTOR.jsx
+++ client/src/pages/PlacementPartnersTOR.jsx
@@ -6,8 +6,8 @@ const PlacementPartnersTOR = () => {
     <>
       <Navbar />
 
-      <main className="pt-24 pb-32 bg-white">
-        <div className="max-w-5xl mx-auto px-6">
+      <main className="pt-30 pb-32 mx-auto bg-white">
+        <div className="max-w-5xl container mx-auto py-20 px-6">
 
           {/* Page Header */}
           <h1 className="text-4xl font-bold mb-2">
